315abb41cb90e068ed88bf4f6de5cefe1ec13fd8
prazhenik.dev <prazhenik.dev@gmail.com>
2022-07-27 20:04:35 +0300

    complete 2005 270722

--- dist/js/app.js
+++ dist/js/app.js
@@ -4652,6 +4652,45 @@
                 playIcon.classList.remove("played");
             }
         }));
+        window.addEventListener("load", windowLoad);
+        function windowLoad() {
+            function digitsCountersInit(digitsCountersItems) {
+                let digitsCounters = digitsCountersItems ? digitsCountersItems : document.querySelectorAll("[data-digits-counter]");
+                if (digitsCounters.length) digitsCounters.forEach((digitsCounter => {
+                    digitsCountersAnimate(digitsCounter);
+                }));
+            }
+            function digitsCountersAnimate(digitsCounter) {
+                let startTimestamp = null;
+                const duration = parseInt(digitsCounter.dataset.digitsCounter) ? parseInt(digitsCounter.dataset.digitsCounter) : 1e3;
+                const startValue = parseInt(digitsCounter.innerHTML);
+                const startPosition = 0;
+                const step = timestamp => {
+                    if (!startTimestamp) startTimestamp = timestamp;
+                    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+                    digitsCounter.innerHTML = Math.floor(progress * (startPosition + startValue));
+                    if (progress < 1) window.requestAnimationFrame(step);
+                };
+                window.requestAnimationFrame(step);
+            }
+            let options = {
+                threshold: .3
+            };
+            let observer = new IntersectionObserver(((entries, observer) => {
+                entries.forEach((entry => {
+                    if (entry.isIntersecting) {
+                        const targetElement = entry.target;
+                        const digitsCountersItems = targetElement.querySelectorAll("[data-digits-counter]");
+                        if (digitsCountersItems.length) digitsCountersInit(digitsCountersItems);
+                        observer.unobserve(targetElement);
+                    }
+                }));
+            }), options);
+            let sections = document.querySelectorAll(".about");
+            if (sections.length) sections.forEach((section => {
+                observer.observe(section);
+            }));
+        }
         window["FLS"] = true;
         isWebp();
         addTouchClass();
